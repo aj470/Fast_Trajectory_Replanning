147baaa381d8197a7d587101a8168f97eb87200b
Dan Snyder <snyderdan46@gmail.com>
2018-02-21 15:03:24 -0500

    now generate a typical maze

--- Fast_Trajectory_Replanning.py
+++ Fast_Trajectory_Replanning.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 
-#Assginment 1: Fast Trajectory Replanning
-#CS 440: Intro to Artificial Intelligence
-#Due date: 26th Feburary 2018
-#authors: Ayush Joshi, Nikhil Mathur, and Dan Snyder
 
-import heapq
+# Assginment 1: Fast Trajectory Replanning
+# CS 440: Intro to Artificial Intelligence
+# Due date: 26th Feburary 2018
+# authors: Ayush Joshi, Nikhil Mathur, and Dan Snyder
+
 import pygame
 from pygame.locals import *
 import os
-import math
-import time
 import signal
 import threading
 from random import *
-from ast import literal_eval as make_tuple
 import sys
 import argparse
 
-#Convention for the Environment
+# Convention for the Environment
 # Initialize Grid
 # 0 is blocked and 1 is unblocked for track_maze
 # 0 is unvisited and 1 is visited for track_maze
@@ -244,6 +241,9 @@ class Node:
     def block(self):
         self._blocked = True
 
+    def unblock(self):
+        self._blocked = False
+
     def visited(self):
         return self._visited
 
@@ -308,6 +308,7 @@ class MazeBuilder:
         self.cols = cols
         self.grid = Grid(rows, cols)
         self.current = None
+        self.clock = pygame.time.Clock()
 
     def init_node(self, node):
         node.visit()
@@ -324,14 +325,13 @@ class MazeBuilder:
         return options[randrange(0, len(options))]
 
     def build(self):
-        self.backtrace = []
+        backtrace = []
         start_x = randrange(0, self.cols)
         start_y = randrange(0, self.rows)
         finished = False
         start_node = self.grid.node(start_x, start_y)
         self.current = start_node
         self.current.visit()
-        self.clock = pygame.time.Clock()
 
         while not finished:
             #self.clock.tick(500)
@@ -341,10 +341,10 @@ class MazeBuilder:
                 finished = True
             elif not next_node:
                 # reached a dead end -- begin back tracking
-                self.current = self.backtrace.pop()
+                self.current = backtrace.pop()
             elif next_node and self.init_node(next_node):
                 # there is an unblocked node to move to -- move to it
-                self.backtrace.append(self.current)
+                backtrace.append(self.current)
                 self.current = next_node
             else:
                 # otherwise, there was a valid neighbor, but it is now blocked
@@ -358,6 +358,46 @@ class MazeBuilder:
                 node.reset()
         print("finished")
         return self.grid
+
+    def build2(self):
+        for row in self.grid.maze:
+            for node in row:
+                node.block()
+
+        backtrace = []
+        start_x = randrange(0, self.cols)
+        start_y = randrange(0, self.rows)
+        finished = False
+        start_node = self.grid.node(start_x, start_y)
+        self.current = start_node
+        self.current.unblock()
+
+        while not finished:
+            #self.clock.tick(60)
+            extended_neighbors = [
+                self.grid.node(self.current.x, self.current.y - 2),
+                self.grid.node(self.current.x, self.current.y + 2),
+                self.grid.node(self.current.x - 2, self.current.y),
+                self.grid.node(self.current.x + 2, self.current.y)
+            ]
+            # get only extended neighbors that exist and are blocked (meaning we can unblock them)
+            extended_neighbors = list(filter(lambda x: x and x.blocked(), extended_neighbors))
+
+            if len(extended_neighbors) == 0 and len(backtrace) == 0:
+                finished = True
+                continue
+            elif len(extended_neighbors) == 0:
+                # if we have no extended neighbors to move to, backtrack
+                self.current = backtrace.pop()
+                continue
+
+            node = extended_neighbors[randrange(0, len(extended_neighbors))]
+            # we can move to this node
+            node.unblock()
+            middle = self.grid.node(int((self.current.x+node.x)/2), int((self.current.y+node.y)/2))
+            middle.unblock()
+            backtrace.append(self.current)
+            self.current = node
 
 
 class LoopingThread(threading.Thread):
@@ -511,7 +551,7 @@ class AgentAlgorithm:
                 return False
 
             for node in path:
-                clock.tick(60)
+                #clock.tick(60)
                 # for each node in the path
                 if node.walkable():
                     self.agent = node
@@ -575,14 +615,14 @@ class DFSAlgorithm(AgentAlgorithm):
         return path
 
 
-
 # having this as a global is kinda weird and makes for weird code
 # I tried to make it not global and couldn't get it quite right so I gave up
 # feel free to try if it becomes inconvenient to you
 mazeBuilder = MazeBuilder(GridRows, GridCols, maze_seed=40)
-algorithm = DFSAlgorithm(mazeBuilder.grid)
+algorithm = RandomAlgorithm(mazeBuilder.grid)
 start = mazeBuilder.grid.node(4, 0)
-end = mazeBuilder.grid.node(97, 100)
+end = mazeBuilder.grid.node(98, 100)
+
 
 class ProcessingThread(LoopingThread):
     def execute(self):
@@ -591,10 +631,10 @@ class ProcessingThread(LoopingThread):
         global start
         global end
 
-        maze = mazeBuilder.build()
+        maze = mazeBuilder.build2()
         algorithm.run(start, end)
-        node = maze.node(4, 0)
         self.stop()
+
 
 def main():
 
@@ -637,161 +677,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
-start_x,start_y,goal_x,goal_y = generateStartFinish()
-final_path = []
-closed_list = []
-cell_costs = []
-priority_list = []
-heuristic_list = []
-path_cost = 0
-elapsed_time = 0
-drawmode = 0
-nodes_expanded = 0
-makeGrid(True)
-start_x,start_y,goal_x,goal_y = 0,0,0,0
-MySearch = AStarSearch() # Initialize Object
-
-while(running):
-	# Get Input
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				running = False
-			elif event.key == pygame.K_g:
-				track_maze = [['1' for y in range(GridRows)] for x in range(GridCols)]
-				areacoordinates = makeGrid()
-				GridSurface = drawGrid(GridSurface)
-				start_x,start_y,goal_x,goal_y = generateStartFinish()
-				final_path = []
-				closed_list = []
-				priority_list = []
-				heuristic_list = []
-				cell_costs = []
-				path_cost = 0
-				elapsed_time = 0
-				nodes_expanded = 0
-				print("Generated new map")
-			elif event.key == pygame.K_e:
-				start_x,start_y,goal_x,goal_y = generateStartFinish()
-				final_path = []
-				closed_list = []
-				priority_list = []
-				heuristic_list = []
-				cell_costs = []
-				path_cost = 0
-				elapsed_time = 0
-				nodes_expanded = 0
-				print("Generated new start and finish points")
-			elif event.key == pygame.K_s:
-				# Save map: get filename
-				filename = raw_input("Save map to: ")
-				with open(os.path.join("./gen",filename),"w") as mapfile:
-					mapfile.write(str((start_x,start_y)))		# Write start
-					mapfile.write("\n")
-					mapfile.write(str((goal_x,goal_y)))			# Write goal
-					mapfile.write("\n")
-
-					for area in areacoordinates:				# Write hard to traverse area centers
-						mapfile.write(str((area[0],area[1])))
-						mapfile.write("\n")
-
-					for y in range(len(track_maze[x])):				# Write each cell
-						for x in range(len(track_maze)):
-							mapfile.write(str(track_maze[x][y]))
-						mapfile.write("\n")
-
-					mapfile.close()
-				print("Map saved!")
-			elif event.key == pygame.K_l:
-				# Load map: get filename
-				filename = raw_input("Load map from: ")
-				with open(os.path.join("./gen",filename),"r") as mapfile: #changed to allow using /maps folder
-					new_start = make_tuple(mapfile.readline())
-					start_x = new_start[0]
-					start_y = new_start[1]
-					new_goal = make_tuple(mapfile.readline())
-					goal_x = new_goal[0]
-					goal_y = new_goal[1]
-
-					areacoordinates = []
-
-					for i in range(8):
-						new_area = make_tuple(mapfile.readline())
-						areacoordinates.append((new_area[0],new_area[1]))
-
-					for y in range(len(track_maze[x])):				# Read each cell
-						for x in range(len(track_maze)):
-							track_maze[x][y] = mapfile.read(1)
-						mapfile.read(1)
-
-					mapfile.close()
-				final_path = []
-				closed_list = []
-				cell_costs = []
-				priority_list = []
-				heuristic_list = []
-				path_cost = 0
-				elapsed_time = 0
-				nodes_expanded = 0
-				GridSurface = drawGrid(GridSurface)
-				print("Map loaded!")
-			elif event.key == pygame.K_UP:
-				if cursor_y-1 >= 0:
-					cursor_y -= 1
-			elif event.key == pygame.K_LEFT:
-				if cursor_x-1 >= 0:
-					cursor_x -= 1
-			elif event.key == pygame.K_RIGHT:
-				if cursor_x+1 < GridCols:
-					cursor_x += 1
-			elif event.key == pygame.K_DOWN:
-				if cursor_y+1 < GridRows:
-					cursor_y += 1
-			elif event.key == pygame.K_v:
-				# draw closed list
-				if drawmode == 0:
-					drawmode = 1
-				else:
-					drawmode = 0
-			elif event.key == pygame.K_a:		# -------- A* Search --------
-				choice = -1
-				while int(choice) < 1 or int(choice) > 6:
-					choice = raw_input ("Enter (1) for Manhattan distance, (2) for Euclidean distance, (3) for Octile distance, (4) for Chebyshev distance, (5) for Straight-Diagonal Distance, or (6) Best/Minimum of all: ")
-				MySearch = AStarSearch()
-				start_time = time.time()
-				closed_list, cell_costs, final_path, path_cost, priority_list, heuristic_list = MySearch.Search(start_x, start_y, goal_x, goal_y,choice)
-				elapsed_time = time.time() - start_time
-				nodes_expanded = len(closed_list)
-			elif event.key == pygame.K_u:		# -------- Uniform Cost Search --------
-				MySearch = UniformCostSearch()
-				start_time = time.time()
-				closed_list, cell_costs, final_path, path_cost, priority_list, heuristic_list = MySearch.Search(start_x, start_y, goal_x, goal_y,1)
-				elapsed_time = time.time() - start_time
-				nodes_expanded = len(closed_list)
-			elif event.key == pygame.K_w:		# -------- Weighted A* Search --------
-				choice = -1 #heuristic choice
-				weight = 0 #weight of heuristic
-
-				while (int(choice) < 1 or int(choice) > 6):
-					choice = raw_input("Enter (1) for Manhattan distance, (2) for Euclidean Distance, (3) for Octile Distance, (4) for Chebyshev Distance, (5) for Straight-Diagonal Distance, or (6) Best/Minimum of all: ")
-				while float(weight) < 1:
-					weight = raw_input("Enter the selected weight for Weighted A* - must be >= 1: ")
-
-				MySearch = WeightedAStarSearch(weight)
-				start_time = time.time()
-				closed_list, cell_costs, final_path, path_cost, priority_list, heuristic_list = MySearch.Search(start_x, start_y, goal_x, goal_y, choice)
-				elapsed_time = time.time() - start_time
-
-				nodes_expanded = len(closed_list)
-			elif event.key == pygame.K_q:		# -------- Sequential A* Search --------
-				MySearch = AStarSearch()
-
-
-		#print(GridSurface,closed_list,final_path,path_cost,nodes_expanded,drawmode,elapsed_time,cell_costs, priority_list, heuristic_list)
-		drawScreen(GridSurface,closed_list,final_path,path_cost,nodes_expanded,drawmode,elapsed_time,cell_costs, priority_list, heuristic_list)
-
-pygame.quit()
-"""
